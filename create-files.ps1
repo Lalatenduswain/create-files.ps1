179b46f519d54d086999d8f54104f2578a7c5138
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-07-07 16:28:54 +0530

    Update create-files.ps1

--- create-files.ps1
+++ create-files.ps1
@@ -8,8 +8,20 @@ for ($i = 1; $i -le $numberOfFiles; $i++) {
     # Generate random content for the file
     $content = Get-Random -Count $fileSizeInBytes -InputObject @(0..255) | ForEach-Object { [char]$_ }
 
-    # Create the file with the specified content
-    New-Item -Path $filePath -ItemType File -Value $content -Force
+    # Convert the content to bytes
+    $contentBytes = [System.Text.Encoding]::Default.GetBytes($content)
+
+    # Determine the number of repetitions needed to reach the desired file size
+    $repetitions = [math]::Ceiling($fileSizeInBytes / $contentBytes.Length)
+
+    # Create the content by repeating the bytes
+    $fileContent = [System.Collections.ArrayList]::new()
+    for ($j = 0; $j -lt $repetitions; $j++) {
+        $fileContent.AddRange($contentBytes)
+    }
+
+    # Write the content to the file
+    [System.IO.File]::WriteAllBytes($filePath, $fileContent.ToArray())
 
     Write-Host "Created file: $fileName"
 }
